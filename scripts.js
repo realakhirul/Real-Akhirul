e1086adf71abd5de8af28b7be2eb590d37ac633e
realakhirul <realakhirul@gmail.com>
2024-09-28 15:13:02 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('loginBtn').addEventListener('click', () => {
+    window.location.href = 'login.html';
+});
+
+document.getElementById('registerBtn').addEventListener('click', () => {
+    window.location.href = 'register.html';
+});
